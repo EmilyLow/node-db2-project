06b3dc8f48927fc539371386300d7e6a0300388b
EmilyLow <emlow@me.com>
2020-10-19 12:34:56 -0500

    check

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      // ?Some place you had to do file NOT from relative location. Here?
+      filename: './data/car_database.db3'
+    },
+    useNullAsDefault: true
+  },
+
+//Not sure I should actually delete all of these
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
