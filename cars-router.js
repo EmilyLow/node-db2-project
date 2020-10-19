54744a8c09c34369ae67cb626f8bd9b975a0f498
EmilyLow <emlow@me.com>
2020-10-19 17:34:53 -0500

    MVP met

--- cars-router.js
+++ cars-router.js
@@ -1,8 +1,40 @@
 const express = require("express");
 
+//!! Don't 100% understand require
 const db = require("./data/config");
 
 const router = express.Router();
+
+//!! Still don't really get 'next'
+router.get("/cars", async (req, res, next) => {
+    
+    try{
+        res.json(await db("cars"))
+    } catch(err) {
+        next (err);
+    }
+    
+})
+
+router.post("/cars", async (req, res, next) => {
+    //Vin, make, model, and mileage
+    try {
+        //Is the returning the id automatic for all db?
+        const [id] = await db("cars").insert(req.body);
+
+        //I believe this pulls it out after you put it in
+        //Not sure about first. First object with ID?
+        const newCar = await db("cars").where({id}).first();
+
+        res.status(201).json(newCar);
+    
+
+    } catch(err) {
+        next(err);
+    }
+
+
+})
 
 
 module.exports = router;
